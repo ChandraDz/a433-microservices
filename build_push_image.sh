728f163c3b1be5c03ce66a3429ba87ddb6bd84f8
ChandraDz <chandrawarrior766@gmail.com>
2024-09-12 08:39:12 +0700

    a

--- build_push_image.sh
+++ build_push_image.sh
@@ -6,5 +6,7 @@ docker build -t item-app:v1 .
 docker images
 # kita akan mengubah nama dari image agar sesuai dengan format
 docker tag item-app:v1 zeandz/item-app:v1
+# kita akan login ke docker hub sebelum login ke docker hub pastikan kalian export password kalian di terminal contoh : export PASSWORD=password_kalian 
+echo $PASSWORD | docker login -u zeandz --password-stdin
 # kita akan push image ke docker hub
-docker push zeandz/item-app:v1 
+docker push zeandz/item-app:v1   
